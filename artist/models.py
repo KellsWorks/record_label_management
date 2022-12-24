e456f2f95598ceaa5e6c8fab7a0544827c3e0c9c
kellsworks <kchidothi@outlook.com>
2022-12-24 11:26:10 +0200

    artist app

--- artist/models.py
+++ artist/models.py
@@ -4,15 +4,22 @@ from user.models import User
 
 class Artist(models.Model):
 
-    GENDER_CHOICES = ("Male", "Female")
+    class GENDER_CHOICES(models.TextChoices):
+        MALE = 'MALE', 'Male'
+        FEMALE = 'FEMALE', 'Female'
 
-    account = models.ForeignKey(User, on_delete=models.CASCADE)
+    account = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user")
     photo = models.ImageField(default="user.png")
     height = models.IntegerField(default=6)
-    gender = models.CharField(max_length=40, default="Male", choices=GENDER_CHOICES)
+    gender = models.CharField(max_length=40, default="Male", choices=GENDER_CHOICES.choices)
     location = models.CharField(max_length=255, null=False)
     date_of_birth = models.DateField(null=False)
     genre = models.ForeignKey(Genre, default=1, on_delete=models.CASCADE)
+    facebook_handle = models.CharField(max_length=255, null=True)
+    instagram_handle = models.CharField(max_length=255, null=True)
+    twitter_handle = models.CharField(max_length=255, null=True)
+    tiktok_handle = models.CharField(max_length=255, null=True)
+
 
     created = models.DateTimeField(auto_now_add=True)
 
--- record_label_management/settings.py
+++ record_label_management/settings.py
@@ -31,6 +31,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'whitenoise.runserver_nostatic',    
     'user',
+    'artist',
     'content',
     'event',
     'schedule',

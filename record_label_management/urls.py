206eeff2da95968d4eae560e71bff404d18a342c
Kelvin Chidothi <56794404+KellsWorks@users.noreply.github.com>
2023-02-04 22:39:35 +0200

    landing page

--- record_label_management/urls.py
+++ record_label_management/urls.py
@@ -1,9 +1,10 @@
+from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
-from django.views.generic import TemplateView
+from django.conf.urls.static import static
 
 urlpatterns = [
+    path('', include('landing.urls')),
     path('admin/', admin.site.urls),
-    path(r'^schedule/', include('schedule.urls')),
-    # path(r'^fullcalendar/', TemplateView.as_view(template_name="fullcalendar.html"), name='fullcalendar'),
-]
+    # path(r'^schedule/', include('schedule.urls')),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
